b41d0134da19480ca472aa90a98b175e92aa1090
SunnyYang <sunnyyang@augmentum.com.cn>
2017-08-21 19:00:55 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="clearfix">
+      <img class="logo" src="./assets/logo.png">
+      <span class="slogan">Vue.js</span>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#app .logo {
+  width: 60px;
+  float: left;
+  margin: 10px 10px 0 0;
+}
+#app .slogan {
+  line-height: 80px;
+  font-size: 40px;
+  float: left;
+}
+div a.navigator-arrow {
+  position: absolute;
+  top: 50%;
+  font-size: 5rem;
+  color: #888888;
+  cursor: pointer;
+}
+.navigator-arrow:hover {
+  color: #42b983;
+  text-decoration: none;
+}
+.navigator-arrow.glyphicon-chevron-left {
+  left: 10px;
+}
+.navigator-arrow.glyphicon-chevron-right {
+  right: 10px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Links from '@/components/Links'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Links',
+      component: Links
+    }
+  ]
+})
